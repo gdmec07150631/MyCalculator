5b1b7bcaebc5edf43ffea9845e9daebb1d4bdb82
07150631 <1255731970@qq.com>
2017-01-02 18:03:49 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150631/mycalculator/MainActivity.java
@@ -0,0 +1,19 @@
+package cn.edu.gdmec.s07150631.mycalculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+/**
+ * Created by acer-pc on 2016/10/8.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void onCreate(Bundle savadInstanceState){
+        super.onCreate(savadInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
